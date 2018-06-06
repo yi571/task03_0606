e20fa62607da0460bf4516c692a150ae8ba7b95e
yi571 <yi571@outlook.com>
2018-06-06 23:03:44 +0800

    新增登入畫面

---
+++ task03_0606/Controllers/MemberController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task03_0606.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Member() {
+            return View();
+        }
+
+        public ActionResult Login() {
+            return View();
+        }
+    }
+}
