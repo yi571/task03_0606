62f8593b5e82c0a14d248de4432014349bd74c0d
yi571_iii <yi571@outlook.com>
2018-06-08 10:19:23 +0800

    欄位確認

--- task03_0606/js/yi.js
+++ task03_0606/js/yi.js
@@ -1,3 +1,21 @@
 ﻿$('#twzipcode').twzipcode({
     'detect': true
 });
+
+$(document).ready(function () {
+    //$("#ConfirmPassword").blur(function () {
+    //    alert("OK")
+    //})
+
+    $("#ConfirmPassword").blur(function () {
+        if ($("#Password1").val() != $("#ConfirmPassword").val()) {
+            $("#ConfPwdLabel").html('確認密碼 <i class="fas fa-times" style="color: red;"></i> 密碼不相符');
+            //$("#ConfirmPassword").tooltip("show");
+        }
+        if ($("#Password1").val() == $("#ConfirmPassword").val()) {
+            $("#ConfPwdLabel").html('確認密碼 <i class="fas fa-check" style="color: green;"></i>');
+            //$("#ConfirmPassword").tooltip("hide");
+        }
+
+    })
+})
