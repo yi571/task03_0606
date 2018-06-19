37b72762f0f09c510da10244f0edb950d4e5d501
5Debbie <wu.debbie.wu@gmail.com>
2018-06-20 07:01:26 +0800

    VIEW updata

--- task03_0606/Controllers/ProductsController.cs
+++ task03_0606/Controllers/ProductsController.cs
@@ -16,7 +16,7 @@ namespace task03_0606.Controllers
     {
 
         FoodProductsEntities db = new FoodProductsEntities();
-        private readonly string base64;
+   
 
         // GET: Products
         public ActionResult Index()
@@ -56,73 +56,82 @@ namespace task03_0606.Controllers
             return View();
         }
 
-       
-    [HttpPost]
-        public ActionResult Create(string productsId, string title,string price,string picture,string introduce)
+        public ActionResult Edit()
         {
-             myfoodproduct mfd = new myfoodproduct()
-            {
-                productId = Convert.ToInt32(productsId),
-                title = title,
-                price = Convert.ToInt32(price),
-                picture = (picture),
-               
-            introduce = introduce,
-                addcount = 12,
-                imageURL = ""
-            };
-
-            //var query = from o in db.myfoodproducts
-            //            select o;
-            //myfoodproduct mfp = query.Single();
-            db.myfoodproducts.Add(mfd);
-            db.SaveChanges();
-            //return Content(productsId);
-            return RedirectToAction("ManagerIndex", "Products");
-        }
-        public ActionResult Upload() {
 
             return View();
-
-        }
-        [HttpPost]
-        public ActionResult Upload(HttpPostedFileBase file)
-        {
-
-            if (file.ContentLength > 0)
-            {
-
-                var fileName = Path.GetFileName(file.FileName);
-
-                var path = Path.Combine(Server.MapPath("~/photo/"), fileName);
-
-                file.SaveAs(path);
-
-            }
-
-            return RedirectToAction("ManagerIndex", "Products");
         }
 
 
+        //[HttpPost]
+        //    public ActionResult Create(string productsId, string title,string price,string picture,string introduce)
+        //    {
+        //         myfoodproduct mfd = new myfoodproduct()
+        //        {
+        //            productId = Convert.ToInt32(productsId),
+        //            title = title,
+        //            price = Convert.ToInt32(price),
+        //            picture = (picture),
+
+        //        introduce = introduce,
+        //            addcount = 12,
+        //            imageURL = ""
+        //        };
+
+
+        //        db.myfoodproducts.Add(mfd);
+        //        db.SaveChanges();
+
+        //        return RedirectToAction("ManagerIndex", "Products");
+        //    } 
+        //var query = from o in db.myfoodproducts
+        //            select o;
+        //myfoodproduct mfp = query.Single();
+        //return Content(productsId);
+
+        //public ActionResult Upload() {
+
+        //    return View();
+
+        //}
+        //[HttpPost]
+        //public ActionResult Upload(HttpPostedFileBase file)
+        //{
+
+        //    if (file.ContentLength > 0)
+        //    {
+
+        //        var fileName = Path.GetFileName(file.FileName);
+
+        //        var path = Path.Combine(Server.MapPath("~/photo/"), fileName);
+
+        //        file.SaveAs(path);
+
+        //    }
+
+        //    return RedirectToAction("ManagerIndex", "Products");
+        //}
 
 
 
-        public ActionResult Edit(int productId)
-        {
-            using (Models.FoodProductsEntities db = new Models.FoodProductsEntities())
-            {
-                var result = (from pro in db.myfoodproducts where pro.productId == productId select pro).FirstOrDefault();
-                if (result != default(Models.myfoodproduct))
-                {
-                    return View(result);
-                }
-                else
-                {
-                    TempData["resultMessage"] = "ERROR";
-                    return RedirectToAction("ManagerIndex");
-                }
-            }
-        }
+
+
+        //public ActionResult Edit(int productId)
+        //{
+        //    using (Models.FoodProductsEntities db = new Models.FoodProductsEntities())
+        //    {
+        //        var result = (from pro in db.myfoodproducts where pro.productId == productId select pro).FirstOrDefault();
+        //        if (result != default(Models.myfoodproduct))
+        //        {
+        //            return View(result);
+        //        }
+        //        else
+        //        {
+        //            TempData["resultMessage"] = "ERROR";
+        //            return RedirectToAction("ManagerIndex");
+        //        }
+        //    }
+        //}
 
         //[HttpPost]
         //public ActionResult Edit(Models.myfoodproduct postback)
@@ -148,44 +157,44 @@ namespace task03_0606.Controllers
         //    }
         //}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "productId,title,price,picture,introduce")]Class1 class1)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(class1).State = System.Data.Entity.EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("ManagerIndex");
-            }
-            return View(class1);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "productId,title,price,picture,introduce")]Class1 class1)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(class1).State = System.Data.Entity.EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("ManagerIndex");
+        //    }
+        //    return View(class1);
+        //}
 
-        [HttpPost]
-        public ActionResult Delete(int productId)
-        {
-            using (Models.FoodProductsEntities db = new Models.FoodProductsEntities())
-            {
-                //抓取Product.Id等於輸入id的資料
-                var result = (from s in db.myfoodproducts where s.productId == productId select s).FirstOrDefault();
-                if (result != default(Models.myfoodproduct)) //判斷此id是否有資料
-                {
-                    db.myfoodproducts.Remove(result);
+        //[HttpPost]
+        //public ActionResult Delete(int productId)
+        //{
+        //    using (Models.FoodProductsEntities db = new Models.FoodProductsEntities())
+        //    {
+        //        //抓取Product.Id等於輸入id的資料
+        //        var result = (from s in db.myfoodproducts where s.productId == productId select s).FirstOrDefault();
+        //        if (result != default(Models.myfoodproduct)) //判斷此id是否有資料
+        //        {
+        //            db.myfoodproducts.Remove(result);
 
-                    //儲存所有變更
-                    db.SaveChanges();
+        //            //儲存所有變更
+        //            db.SaveChanges();
 
-                    //設定成功訊息並導回index頁面
-                    TempData["ResultMessage"] = string.Format("商品[{0}]成功刪除", result.title);
-                    return RedirectToAction("ManagerIndex");
-                }
-                else
-                {   //如果沒有資料則顯示錯誤訊息並導回Index頁面
-                    TempData["resultMessage"] = "指定資料不存在，無法刪除，請重新操作";
-                    return RedirectToAction("ManagerIndex");
-                }
-            }
-        }
+        //            //設定成功訊息並導回index頁面
+        //            TempData["ResultMessage"] = string.Format("商品[{0}]成功刪除", result.title);
+        //            return RedirectToAction("ManagerIndex");
+        //        }
+        //        else
+        //        {   //如果沒有資料則顯示錯誤訊息並導回Index頁面
+        //            TempData["resultMessage"] = "指定資料不存在，無法刪除，請重新操作";
+        //            return RedirectToAction("ManagerIndex");
+        //        }
+        //    }
+        //}
 
         //[HttpPost]
         //public ActionResult Edit(int productId)
