ccc83ae1410347a095d6815ad0ea7cf96163aeb8
5Debbie <39683219+5Debbie@users.noreply.github.com>
2018-06-19 18:09:44 +0800

    updata Edit VIEW

--- task03_0606/js/Aindex.js
+++ task03_0606/js/Aindex.js
@@ -318,4 +318,72 @@ function showMyImage(fileInput) {
         reader.readAsDataURL(file);
     }
 }
-//--------------------
+//--------------------Edit UI
+
+$("textarea").blur(function () {
+    $("#hire textarea").each(function () {
+        $this = $(this);
+        if (this.value != "") {
+            $this.addClass("focused");
+            $("textarea + label + span").css({ opacity: 1 });
+        } else {
+            $this.removeClass("focused");
+            $("textarea + label + span").css({ opacity: 0 });
+        }
+    });
+});
+
+$("#hire .field:first-child input").blur(function () {
+    $("#hire .field:first-child input").each(function () {
+        $this = $(this);
+        if (this.value != "") {
+            $this.addClass("focused");
+            $(".field:first-child input + label + span").css({ opacity: 1 });
+        } else {
+            $this.removeClass("focused");
+            $(".field:first-child input + label + span").css({ opacity: 0 });
+        }
+    });
+});
+
+$("#hire .field:nth-child(2) input").blur(function () {
+    $("#hire .field:nth-child(2) input").each(function () {
+        $this = $(this);
+        if (this.value != "") {
+            $this.addClass("focused");
+            $(".field:nth-child(2) input + label + span").css({ opacity: 1 });
+        } else {
+            $this.removeClass("focused");
+            $(".field:nth-child(2) input + label + span").css({ opacity: 0 });
+        }
+    });
+});
+
+
+//material contact form animation-----------UI Create
+
+$('.contact-form').find('.form-control').each(function () {
+    var targetItem = $(this).parent();
+    if ($(this).val()) {
+        $(targetItem).find('label').css({
+            'top': '10px',
+            'fontSize': '14px'
+        });
+    }
+})
+$('.contact-form').find('.form-control').focus(function () {
+    $(this).parent('.input-block').addClass('focus');
+    $(this).parent().find('label').animate({
+        'top': '10px',
+        'fontSize': '14px'
+    }, 300);
+})
+$('.contact-form').find('.form-control').blur(function () {
+    if ($(this).val().length == 0) {
+        $(this).parent('.input-block').removeClass('focus');
+        $(this).parent().find('label').animate({
+            'top': '25px',
+            'fontSize': '18px'
+        }, 300);
+    }
+})
